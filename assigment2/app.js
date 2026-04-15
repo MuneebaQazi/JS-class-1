c6b9e6670ccb86275daee5598285e20cf5247856
MuneebaQazi <muneebaqazi35@gmail.com>
2026-04-15 18:37:08 +0500

    assignment2

---
+++ assigment2/app.js
@@ -0,0 +1,24 @@
+// question # 1
+// let userName;
+
+// question # 2
+// let myName = "Muneeba Qazi"
+
+// question # 3
+// let message = "Hello World"
+
+// question # 4 
+// alert(message)
+
+// question # 5
+// alert("Pizza\nPizz\nPiz\npi")
+
+// // question # 6
+// alert("My email address is alisjo@34gmail.com")
+
+// question # 7
+// alert{"A smarter way to learn java script"}
+
+// question # 8 
+// document.write("Yah! I can write HTML content throught javascript" )
+
