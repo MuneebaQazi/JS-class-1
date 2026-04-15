f75288b5d36e29573b3af2fecf71f1a060c064a9
MuneebaQazi <muneebaqazi35@gmail.com>
2026-04-15 18:16:20 +0500

    new editing

---
+++ assignment/app.js
@@ -0,0 +1,10 @@
+// question:1
+// alert("Error! Please enter a valid password.")
+// question2
+// alert("Welcome to JS Land...\n Happy Coding!")
+// question3
+// alert("Welcome to JS Land...")
+// question4
+// alert("Happy Coding!\n Prevent this page from creating additional dialogs.")
+// question5
+// alert("Hello...I canrun JS throughtmy web brower's console")
