f2fee3451d3438901e3911dda92f23bdcbd783fc
Muneebaqazi <muneebaqazi35@gmail.com>
2026-04-15 21:24:13 +0500

    Assignment3

---
+++ assignment3/app.js
@@ -0,0 +1,11 @@
+// Question # 1
+// alert("I am 15 years old.");
+
+// Question # 2 
+// alert("You have visited this site 14 times");
+
+// Question # 3
+// alert("My birthday year is 2010\nData type of my ddeclared variable is number");
+
+// Question # 4
+// alert("John Doe ordered 5 T-Shirt(s) on XYZ Clothing Store.");
