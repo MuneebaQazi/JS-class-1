a43811faab5e0eb45167c7eb7c3181502df80388
Muneebaqazi <muneebaqazi35@gmail.com>
2026-04-16 18:41:15 +0500

    assigment # 3

---
+++ assignment4/app.js
@@ -0,0 +1,30 @@
+// Question # 1 
+// var name = "Muneeba", age = 15, city = "Karachi";
+
+// Question # 2
+// legal Variables Name.
+// var name;        // simple variable, usually text store karta hai
+// var user1;       // number ke sath letter allowed hai (but start letter se hota hai)
+// var myVariable;  // camelCase style, commonly use hota hai
+// var $price;      // $ sign allowed hai 
+// var total_amount;// underscore (_) allowed hai,
+// illegal variables Names.
+// var 1name;      number se start
+// var user name;  space hai
+// var @value;     special character
+// var var;        reserved word
+// var my-name;    dash allowed nahi
+  
+
+
+// Queston # 3
+
+// document.write("<strong>A heading stating Rules for naming JS variables</strong><br><br>");
+
+// document.write("Variables names can only contain numbers, $ and _. For example: $my_1stVariable<br><br>");
+
+// document.write("Variables must begin with a letter, $ or _. For example: $name, _name or name<br><br>");
+
+// document.write("Variables names are case sensitive<br><br>");
+
+// document.write("Variables names should not be JS keywords");
