21d90a22376f4326f7843aeaa1cfc0a4ad51b971
saadjunaid54321-ait <muneebaqazi35@gmail.com>
2026-04-23 14:01:59 +0500

    assignment5

---
+++ assignment5/app.js
@@ -0,0 +1,68 @@
+// Question no # 1
+// let num1 = 3;
+// let num2 = 5;
+// let sum = num1 + num2 ;
+// document.write(sum)
+
+// Question no # 2
+// let num1 = 3;
+// let num2 = 5; 
+
+// Subtraction
+// let sub = num1 - num2;
+// document.write(sub)
+
+// Multipilication
+// let multi =  num1 * num2;
+// document.write(multi)
+
+// Division 
+// let divide = num1 / num2;
+// document.write(divide)
+
+// Modulus
+// let modulus = num1 % num2;
+// document.write(modulus)
+
+
+// Question no # 3
+// let myNumber;undefined;
+// document.write("Value after variable declaration  is " + myNumber + "<br>")
+
+// let num = 5;
+// document.write("Initial Value:" + num + "<br>")
+
+// let num2 = 6;
+// document.write("Value after increment is:" + num2 + "<br>")
+
+// let num3 = 13;
+// document.write("Value after addition is:" + num3 + "<br>")
+
+// let num4 = 12;
+// document.write("Value after decrement is:" + num4 + "<br>")
+
+// let num5 = 0;
+// document.write("The remainder is:" + num5 + "<br>")
+
+
+// Question no # 4
+// let ticketPrice = 600;
+// let numberOfTickets = 5;
+// let totalCost = ticketPrice * numberOfTickets;
+
+// document.write("Totle cost to buy 5 ticket to a movie is " + totalCost + "PKR")
+
+
+// Question no # 5
+// let num = 4;
+// document.write("Table of " + num + "<br>")
+// document.write(num + "X1=" + num*1 + "<br>")
+// document.write(num + "X2=" + num*2 + "<br>")
+// document.write(num + "X3=" + num*3 + "<br>")
+// document.write(num + "X4=" + num*4 + "<br>")
+// document.write(num + "X5=" + num*5 + "<br>")
+// document.write(num + "X6=" + num*6 + "<br>")
+// document.write(num + "X7=" + num*7 + "<br>")
+// document.write(num + "X8=" + num*8 + "<br>")
+// document.write(num + "X9=" + num*9 + "<br>")
+// document.write(num + "X10=" + num*10 + "<br>")
