adddc50262fd9dbe1736121fdd08214688b18ef4
saadjunaid54321-ait <muneebaqazi35@gmail.com>
2026-04-28 18:45:50 +0500

    assignment5 complete

--- assignment5/app.js
+++ assignment5/app.js
@@ -66,3 +66,92 @@
 // document.write(num + "X8=" + num*8 + "<br>")
 // document.write(num + "X9=" + num*9 + "<br>")
 // document.write(num + "X10=" + num*10 + "<br>")
+
+
+// Question # 6
+// let celsius = 25;
+// let fahrenheit = (celsius * 9/5) + 32;
+// document.write(celsius + "C is " + fahrenheit + "F <br>");
+
+// let fahrenheit2 = 70;
+// let celsius2 = (fahrenheit2 - 32) * 5/9;
+// document.write(fahrenheit2 + "F is " + celsius2 + "C");
+
+
+// Question # 7 
+// let priceItem1 = 650;
+// let priceItem2 = 100;
+// let qtyItems1 = 3;
+// let qtyItems2 = 7;
+// let shoppingCharges = 100; 
+// let totalCost = (priceItem1 * qtyItems1) + (priceItem2 * qtyItems2) + shoppingCharges;
+// document.write("<h1>Shopping Cart</h1>");
+// document.write("price of item 1 is " + priceItem1 + "<br>");
+// document.write("Quantity of item 1 is" + qtyItems1 + "<br>");
+// document.write("price of item 2 is " + priceItem2 + "<br>");
+// document.write("Quantity of item 2 is " +qtyItems2 + "<br>");
+// document.write("Shopping Charges" + shoppingCharges + "<br><br>");
+// document.write("Total cost of your order is " + totalCost);
+
+
+// Questipon # 8
+// let totalMarks = 980;
+// let marksObtained = 804;
+// let percentage = (marksObtained / totalMarks) * 100;
+
+// document.write("<h1>Marks Sheet</h1><br>");
+// document.write("Total marks: " + totalMarks + "<br>");
+// document.write("Marks obtained: " + marksObtained + "<br>");
+// document.write("Percentage: " + percentage + "%");
+
+
+
+// Question # 9
+// let totalCurrencyInPKR = (10 * 104.80) + (25 * 28);
+// document.write("<h1>Currency in PKR</h1><br>");
+// document.write("Total Currency in PKR: " + totalCurrencyInPKR);
+
+
+// Question # 10
+// let number = 7;
+// let sum = 5+7;
+// let multi = sum * 10
+// let divide = multi/2;
+// let result = (7+5) * 10 / 2; 
+// document.write(result)
+
+
+
+// Question # 11
+// let currentYear = 2026;
+// let birthday = 2010;
+// let age1 = currentYear - birthday;
+// let age2 = currentYear - birthday - 1;
+// document.write("<h1>The Age Calculator</h1>");
+// document.write("They are either " + age1 + " or " + age2 + " year old");
+
+
+
+
+// Question # 12
+        // let radius = 20;
+        // let pi = 3.142;
+        // let circumference = 2 * pi * radius;
+        // let area = pi * radius * radius;
+        // document.write("<h1>The Geometrizier</h1>");
+        // document.write("The circumference is " + circumference + "<br>");
+        // document.write("The area is " + area);
+
+
+        // Question # 13
+//         let favoriteSnack = "Chocolate Chips";
+//         let currentAge = 15;
+//         let maxAge = 65;
+//         let amountPerDay = 3;
+//         let yearsLeft = maxAge - currentAge;
+//         document.write("<h1>The Lifetime Supply Calculator</h1>");
+// document.write("Favourite Snack: chocalate chip <br>");
+// document.write("Current age :" + currentAge + "<br>");
+// document.write("Estamated maximum Age :" + maxAge + "<br>");
+// document.write("Amount of snacke per day : " + amountPerDay + "<br>");
+// document.write("You will need 150 chocalate chip to last you until the ripe old age  of " + yearsLeft + "<br>");
